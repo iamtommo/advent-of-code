3a2bf81f179fb7aef6317ee975729c7e2d7c7f48
thomas.madsen <thomas.madsen@egym.de>
2015-12-03 15:49:22 +0100

    Remove autogen func

--- src/cloj_test/core.clj
+++ src/cloj_test/core.clj
@@ -1,10 +1,5 @@
 (ns cloj-test.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn advent-of-code-1
   [floors]
   (reduce + (map #(cond (= \( %) 1 (= \) %) -1 :else 0) floors)))
